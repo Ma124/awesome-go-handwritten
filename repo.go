793b633eeb1c40006350d6e36cf9bd28c8cba52b
Ma_124 <ma_124@pm.me>
2019-01-22 20:42:04 +0100

    finishing meta-tables (with css etc.)

--- repo.go
+++ repo.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"encoding/json"
+	"flag"
 	"io"
 	"io/ioutil"
 	"net/http"
@@ -21,6 +22,7 @@ import (
 // memory usage optimizations
 const (
 	emtyStr  = ""
+	maxInt   = int((^uint(0)) >> 1)
 	git      = "git"
 	checkout = "checkout"
 	force    = "-f"
@@ -45,18 +47,25 @@ type ghJson struct {
 	Forks      int    `json:"forks_count"`
 	Issues     int    `json:"open_issues_count"`
 	LastCommit string `json:"pushed_at"`
+	Message    string `json:"message"`
 }
+
+var githubToken = ""
+var maxReqCount = 0
+var noCheckout = false
 
 func generateHTML() {
 	// Update repo
-	_, err := exec.Command(git, checkout, force).Output()
-	if err != nil {
-		panic(err)
-	}
+	if !noCheckout {
+		_, err := exec.Command(git, checkout, force).Output()
+		if err != nil {
+			panic(err)
+		}
 
-	_, err = exec.Command(git, pull).Output()
-	if err != nil {
-		panic(err)
+		_, err = exec.Command(git, pull).Output()
+		if err != nil {
+			panic(err)
+		}
 	}
 
 	f, err := os.Open(readmePath)
@@ -65,8 +74,7 @@ func generateHTML() {
 	}
 	defer f.Close()
 
-	input := bufio.NewReader(f)
-	body := string(gfm.Markdown([]byte(processReadme(input))))
+	body := string(gfm.Markdown([]byte(processReadme(bufio.NewReader(f)))))
 	c := &content{Body: body}
 
 	t := template.Must(template.ParseFiles(tplPath))
@@ -76,16 +84,17 @@ func generateHTML() {
 	}
 	defer f2.Close()
 
-	err = t.Execute(f, c)
+	err = t.Execute(f2, c)
 	if err != nil {
 		panic(err)
 	}
 }
 
 func processReadme(r *bufio.Reader) string {
-	buf := strings.Builder{}
+	buf := &strings.Builder{}
 	writeTblHead := true
 	i := 0
+	reqCounts := 0
 	for {
 		l, err := r.ReadString('\n')
 		if err != nil {
@@ -95,70 +104,82 @@ func processReadme(r *bufio.Reader) string {
 			panic(err)
 		}
 
-		println(i)
 		i++
 
-		if !(len(l) > 2 && l[0] == '*' && l[1] != ' ') {
-			writeTblHead = true
-			buf.WriteString(l)
-		} else {
+		starI := strings.IndexByte(l, '*')
+
+		if starI != -1 && (l[0] == ' ' || l[0] == '*') && strings.HasPrefix(l[starI:], "* [") {
 			if writeTblHead {
-				buf.WriteString("| Stars | Forks | Open Issues | Last Commit | Name | Description |\n")
+				writeTblHead = false
+				buf.WriteString("\n| Stars | Forks | Open Issues | Last Commit | Name | Description |\n")
 				buf.WriteString("|-------|-------|-------------|-------------|------|-------------|\n")
-				// stargazers_count forks_count pushed_at open_issues_count
-				afterNameI := strings.LastIndexByte(l, ']')
-				name := l[4:afterNameI]
-				println(name)
-				desc := l[strings.Index(l, " - ")+1:len(l)-1]
-				url := l[afterNameI+2 : strings.LastIndexByte(l, ')')]
-				if strings.HasPrefix(l, "https://github.com/") {
-					url = url[20:]
-					resp, err := http.Get("https://api.github.com/repos/" + url)
+			}
+			// stargazers_count forks_count pushed_at open_issues_count
+			afterNameI := strings.IndexByte(l, ']')
+			name := l[starI+3 : afterNameI]
+			desc := l[strings.Index(l, " - ")+3 : len(l)-1]
+			url := l[afterNameI+2 : strings.IndexByte(l[afterNameI+2:], ')')+afterNameI+2]
+			if strings.HasPrefix(url, "https://github.com/") {
+				var data []byte
+				reqCounts++
+				if reqCounts < maxReqCount {
+					resp, err := http.Get("https://api.github.com/repos/" + url[19:] + "?access_token=" + githubToken)
 					if err != nil {
 						panic(err)
 					}
 
-					data, err := ioutil.ReadAll(resp.Body)
+					data, err = ioutil.ReadAll(resp.Body)
 					if err != nil {
 						panic(err)
 					}
 					resp.Body.Close()
+				} else {
+					data = []byte(`{ "stargazers_count": 1234, "forks_count": 56, "open_issues_count": 7, "pushed_at": "2019-01-20T19:24:24Z" }`)
+				}
 
-					jsO := &ghJson{}
-					err = json.Unmarshal(data, jsO)
-					if err != nil {
-						panic(err)
-					}
+				jsO := &ghJson{}
+				err = json.Unmarshal(data, jsO)
+				if err != nil {
+					panic(err)
+				}
 
-					buf.WriteString("| ")
-					buf.WriteString(strconv.Itoa(jsO.Stars))
-					buf.WriteString(":star: | ")
-					buf.WriteString(strconv.Itoa(jsO.Forks))
-					buf.WriteString(" | ")
-					buf.WriteString(strconv.Itoa(jsO.Issues))
-					buf.WriteString(" | ")
-
+				if jsO.Message != "" {
+					WriteTableColumns(buf, "N/A", "N/A", "N/A", "N/A", name, url, desc)
+				} else {
 					t, err := time.Parse(time.RFC3339, jsO.LastCommit)
 					if err != nil {
 						panic(err)
 					}
-					buf.WriteString(timeago.English.Format(t))
 
-					buf.WriteString(name)
-					buf.WriteString(" | ")
-					buf.WriteString(desc)
-					buf.WriteString(" |")
-				} else {
-					buf.WriteString("| N/A | N/A | N/A | N/A | ")
-					buf.WriteString(name)
-					buf.WriteString(" | ")
-					buf.WriteString(desc)
-					buf.WriteString(" |")
+					WriteTableColumns(buf, strconv.Itoa(jsO.Stars), strconv.Itoa(jsO.Forks), strconv.Itoa(jsO.Issues), timeago.English.Format(t), name, url, desc)
 				}
+			} else {
+				WriteTableColumns(buf, "N/A", "N/A", "N/A", "N/A", name, url, desc)
 			}
+		} else {
+			writeTblHead = true
+			buf.WriteString(l)
 		}
 	}
 	return buf.String()
+}
+
+func WriteTableColumns(buf *strings.Builder, stars, forks, openIssues, lastCommit, name, url, desc string) {
+	buf.WriteString("| ")
+	buf.WriteString(stars)
+	buf.WriteString(" | ")
+	buf.WriteString(forks)
+	buf.WriteString(" | ")
+	buf.WriteString(openIssues)
+	buf.WriteString(" | ")
+	buf.WriteString(lastCommit)
+	buf.WriteString(" | [")
+	buf.WriteString(name)
+	buf.WriteString("](")
+	buf.WriteString(url)
+	buf.WriteString(") | ")
+	buf.WriteString(desc)
+	buf.WriteString(" |\n")
 }
 
 func hookHandler(w http.ResponseWriter, r *http.Request) {
@@ -167,17 +188,24 @@ func hookHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	if len(os.Args) == 2 {
-		if os.Args[1] == "compile" {
-			generateHTML()
-			return
-		} else if os.Args[1] == "hook-server" {
-		} else {
-			println("Unknown command")
-			return
-		}
+	compile := false
+	flag.BoolVar(&compile, "compile", false, "Don't launch a server and just compile it once")
+	flag.StringVar(&githubToken, "gh-token", "", "Generate one at https://github.com/settings/tokens")
+	flag.IntVar(&maxReqCount, "max-req-count", maxInt, "Only for development.")
+	flag.BoolVar(&noCheckout, "no-checkout", false, "Don't run `git checkout force`")
+
+	flag.Parse()
+
+	if githubToken == "" {
+		panic("You need to specify a --gh-token")
 	}
-	r := mux.NewRouter()
-	r.HandleFunc("/hook", hookHandler)
-	http.ListenAndServe(":9000", r)
+
+	if compile {
+		generateHTML()
+		return
+	} else {
+		r := mux.NewRouter()
+		r.HandleFunc("/hook", hookHandler)
+		http.ListenAndServe(":9000", r)
+	}
 }
