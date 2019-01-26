683f295908cef726dde150c2bc5f9e980775fa95
Ma_124 <ma_124@pm.me>
2019-01-26 19:10:37 +0100

    support for gitlab.com

--- repo.go
+++ repo.go
@@ -50,15 +50,8 @@ type content struct {
 	Body string
 }
 
-type ghJson struct {
-	Stars      int    `json:"stargazers_count"`
-	Forks      int    `json:"forks_count"`
-	Issues     int    `json:"open_issues_count"`
-	LastCommit string `json:"pushed_at"`
-	Message    string `json:"message"`
-}
-
 var githubToken = ""
+var gitlabToken = ""
 var maxReqCount = 0
 var noCheckout = false
 
@@ -130,21 +123,40 @@ func processReadme(r *bufio.Reader) string {
 				buf.WriteString("\n| Stars | Forks | Open Issues | Last Commit | Name | Description |\n")
 				buf.WriteString("|-------|-------|-------------|-------------|------|-------------|\n")
 			}
-			// stargazers_count forks_count pushed_at open_issues_count
 			afterNameI := strings.IndexByte(l, ']')
 			name := l[starI+3 : afterNameI]
 			desc := l[strings.Index(l, " - ")+3 : len(l)-1]
 			url := l[afterNameI+2 : strings.IndexByte(l[afterNameI+2:], ')')+afterNameI+2]
 			if strings.HasPrefix(url, "https://github.com/") {
-				reqCounts = fetchAndWriteGitHub(buf, name, url, desc, reqCounts, 19)
+				reqCounts = fetchAndWriteGitHub(buf, name, url, desc, reqCounts, url[19:])
 			} else if strings.HasPrefix(url, "https://godoc.org/github.com/") {
-				reqCounts = fetchAndWriteGitHub(buf, name, url, desc, reqCounts, 29) // TODO test n' fix
+				reqCounts = fetchAndWriteGitHub(buf, name, url, desc, reqCounts, url[29:])
 			} else if strings.HasPrefix(url, "http://github.com/") {
-				reqCounts = fetchAndWriteGitHub(buf, name, url, desc, reqCounts, 18) // TODO test n' fix
+				reqCounts = fetchAndWriteGitHub(buf, name, url, desc, reqCounts, url[18:])
 			} else if strings.HasPrefix(url, "http://godoc.org/github.com/") {
-				reqCounts = fetchAndWriteGitHub(buf, name, url, desc, reqCounts, 28) // TODO test n' fix
+				reqCounts = fetchAndWriteGitHub(buf, name, url, desc, reqCounts, url[28:])
+			} else if strings.HasPrefix(url, "https://gitlab.com/") {
+				reqCounts = fetchAndWriteGitLab(buf, name, url, desc, reqCounts, strings.Replace(url[19:], "/", "%2F", -1))
+			} else if strings.HasPrefix(url, "http://gitlab.com/") {
+				reqCounts = fetchAndWriteGitLab(buf, name, url, desc, reqCounts, strings.Replace(url[18:], "/", "%2F", -1))
 			} else {
-				WriteTableColumns(buf, "N/A", "N/A", "N/A", "N/A", name, url, desc)
+				dotI := strings.IndexByte(url, '.')
+				postDot := url[dotI+1:]
+
+				if strings.HasPrefix(postDot, "github.io") {
+					slashI := strings.IndexByte(postDot, '/')
+					if slashI > 0 {
+						if url[7] == '/' {
+							reqCounts = fetchAndWriteGitHub(buf, name, url, desc, reqCounts, url[8:dotI]+"/"+postDot[slashI+1:])
+						} else {
+							reqCounts = fetchAndWriteGitHub(buf, name, url, desc, reqCounts, url[7:dotI]+"/"+postDot[slashI+1:])
+						}
+					} else {
+						WriteNATableColumns(buf, name, url, desc)
+					}
+				} else {
+					WriteNATableColumns(buf, name, url, desc)
+				}
 			}
 		} else {
 			writeTblHead = true
@@ -165,11 +177,23 @@ func printProgressBar() {
 	pb.Set(int(offset))
 }
 
-func fetchAndWriteGitHub(buf *strings.Builder, name, url, desc string, reqCounts int, urlOffset int) (reqCountz int) {
+type ghJson struct {
+	Stars      int    `json:"stargazers_count"`
+	Forks      int    `json:"forks_count"`
+	Issues     int    `json:"open_issues_count"`
+	LastCommit string `json:"pushed_at"`
+	Message    string `json:"message"`
+}
+
+func fetchAndWriteGitHub(buf *strings.Builder, name, url, desc string, reqCounts int, apiUrl string) (reqCountz int) {
 	var data []byte
 
+	if url[len(url)-1] == '/' {
+		url = url[:len(url)-1]
+	}
+
 	if reqCounts < maxReqCount {
-		resp, err := http.Get("https://api.github.com/repos/" + url[urlOffset:] + "?access_token=" + githubToken)
+		resp, err := http.Get("https://api.github.com/repos/" + apiUrl + "?access_token=" + githubToken)
 		if err != nil {
 			panic(err)
 		}
@@ -190,7 +214,7 @@ func fetchAndWriteGitHub(buf *strings.Builder, name, url, desc string, reqCounts
 	}
 
 	if jsO.Message != "" {
-		WriteTableColumns(buf, "N/A", "N/A", "N/A", "N/A", name, url, desc)
+		WriteNATableColumns(buf, name, url, desc)
 	} else {
 		t, err := time.Parse(time.RFC3339, jsO.LastCommit)
 		if err != nil {
@@ -198,6 +222,56 @@ func fetchAndWriteGitHub(buf *strings.Builder, name, url, desc string, reqCounts
 		}
 
 		WriteTableColumns(buf, strconv.Itoa(jsO.Stars), strconv.Itoa(jsO.Forks), strconv.Itoa(jsO.Issues), timeago.English.Format(t), name, url, desc) // TODO fork and improve timeago
+	}
+
+	return reqCounts + 1
+}
+
+type glJson struct {
+	Stars      int    `json:"star_count"`
+	Forks      int    `json:"forks_count"`
+	Issues     int    `json:""` // len(.../issues)
+	LastCommit string `json:"last_activity_at"`
+	Message    string `json:"message"`
+}
+
+func fetchAndWriteGitLab(buf *strings.Builder, name, url, desc string, reqCounts int, apiUrl string) (reqCountz int) {
+	var data []byte
+
+	if url[len(url)-1] == '/' {
+		url = url[:len(url)-1]
+	}
+
+	if reqCounts < maxReqCount {
+		resp, err := http.Get("https://gitlab.com/api/v4/projects/" + apiUrl + "?access_token=" + gitlabToken)
+		if err != nil {
+			panic(err)
+		}
+
+		data, err = ioutil.ReadAll(resp.Body)
+		if err != nil {
+			panic(err)
+		}
+		resp.Body.Close()
+	} else {
+		data = []byte(`{ "star_count": 1234, "forks_count": 56, "": 7, "last_activity_at": "2019-01-20T19:24:24Z" }`)
+	}
+
+	jsO := &glJson{}
+	err := json.Unmarshal(data, jsO)
+	if err != nil {
+		panic(err)
+	}
+
+	if jsO.Message != "" {
+		WriteNATableColumns(buf, name, url, desc)
+	} else {
+		t, err := time.Parse(time.RFC3339, jsO.LastCommit)
+		if err != nil {
+			panic(err)
+		}
+
+		WriteTableColumns(buf, strconv.Itoa(jsO.Stars), strconv.Itoa(jsO.Forks), "N/A", timeago.English.Format(t), name, url, desc) // TODO fork, improve timeago and issues
 	}
 
 	return reqCounts + 1
@@ -221,6 +295,10 @@ func WriteTableColumns(buf *strings.Builder, stars, forks, openIssues, lastCommi
 	buf.WriteString(" |\n")
 }
 
+func WriteNATableColumns(buf *strings.Builder, name, url, desc string) {
+	WriteTableColumns(buf, "N/A", "N/A", "N/A", "N/A", name, url, desc)
+}
+
 func hookHandler(w http.ResponseWriter, r *http.Request) {
 	go generateHTML()
 	w.Write(doneResp)
@@ -230,6 +308,7 @@ func main() {
 	compile := false
 	flag.BoolVar(&compile, "compile", false, "Don't launch a server and just compile it once")
 	flag.StringVar(&githubToken, "gh-token", "", "Generate one at https://github.com/settings/tokens")
+	flag.StringVar(&gitlabToken, "gl-token", "", "Generate one at https://gitlab.com/profile/applications")
 	flag.IntVar(&maxReqCount, "max-req-count", maxInt, "Only for development.")
 	flag.BoolVar(&noCheckout, "no-checkout", false, "Don't run `git checkout force`")
 
